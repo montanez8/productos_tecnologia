81b10893ea4fefc1bb8591afe6e54df14ed1b237
montanez8 <montanezcompany.r@gmail.com>
2024-01-19 14:16:29 -0500

    add class Producto and class Inventario

--- src/Inventario.java
+++ src/Inventario.java
@@ -33,4 +33,45 @@ public class Inventario {
         productos.remove(numeroReferencia);
     }
 
+    // metodo para buscar un producto en el inventario
+    public Producto buscarProducto(int numeroReferencia) {
+        if (!productos.containsKey(numeroReferencia)) {
+            throw new IllegalArgumentException("El producto no existe en el inventario.");
+        }
+        return productos.get(numeroReferencia);
+    }
+
+    // metodo para add nueva categoria
+    public void agregarCategoria(Categoria categoria) {
+        if (categorias.containsValue(categoria)) {
+            throw new IllegalArgumentException("La categoria ya existe en el inventario.");
+        }
+        categorias.put(categorias.size() + 1, categoria);
+    }
+
+    // metodo para eliminar una categoria
+    public void eliminarCategoria(int numeroCategoria) {
+        if (!categorias.containsKey(numeroCategoria)) {
+            throw new IllegalArgumentException("La categoria no existe en el inventario.");
+        }
+        categorias.remove(numeroCategoria);
+    }
+
+
+    // listar productos por categoria
+    public void listarProductosPorCategoria(Categoria categoria) {
+        for (Map.Entry<Integer, Producto> entry : productos.entrySet()) {
+            if (entry.getValue().getCategoria() == categoria) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+
+    // listar categorias
+    public void listarCategorias() {
+        for (Map.Entry<Integer, Categoria> entry : categorias.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
 }
